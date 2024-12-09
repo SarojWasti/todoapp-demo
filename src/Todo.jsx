1161078fd6622f6e2b984562d8b60a7aadc660c8
SarojWasti <sarozwosti@gmail.com>
2024-12-09 16:38:06 -0500

    Test jenkins

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -21,7 +21,7 @@ const Todo = () => {
   };
   return (
     <div>
-      <h1>TodoList DevOps : Auto Deploymeent for Kubernetes Class Test</h1>
+      <h1>TodoList DevOps : Auto Deploymeent - Test Jenkins</h1>
       <div className="inputDiv">
         <input
           value={value}
