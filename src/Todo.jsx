01529664fe4f4113ee386eb657a82d997db3c9f6
SarojWasti <sarozwosti@gmail.com>
2024-12-09 18:49:14 -0500

    Push to docker

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -21,7 +21,7 @@ const Todo = () => {
   };
   return (
     <div>
-      <h1>TodoList DevOps - Auto Deploymeent</h1>
+      <h1>TodoList DevOps</h1>
       <div className="inputDiv">
         <input
           value={value}
